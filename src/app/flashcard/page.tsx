7523c05a3955f4faac49ef3751ad06c9a063bb49
vupadhyayula8 <vupadhyayula@binghamton.edu>
2024-08-13 18:22:54 -0400

    landing page with google auth

---
+++ src/app/flashcard/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Box, Button, Stack, Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <div>
+        <span>Welcome</span>
+    </div>
+  );
+}
